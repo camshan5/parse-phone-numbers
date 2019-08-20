9486161aae75619d7ac6d01f81803e0ecf315c35
Cameron Shannon <cameroncshannon@gmail.com>
2019-08-20 10:56:23 -0700

    Completed postgres db setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,9 +1,21 @@
 from flask.cli import FlaskGroup
-from project import app
+from project import app, db
 
 # created a new FlaskGroup instance to extend the normal
 # CLI with commands related to the Flask app.
 cli = FlaskGroup(app)
+
+
+@cli.command("recreate_db")
+def recreate_db():
+    """
+    Registers to the CLI so it can run from the command line
+    to apply the model to the database
+    """
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 if __name__ == "__main__":
     cli()
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -10,8 +11,24 @@ api = Api(app)
 
 
 # set config
-app_settings = os.getenv('APP_SETTINGS')
+app_settings = os.getenv("APP_SETTINGS")
 app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    phone_number = db.Column(db.String(255), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, phone_number):
+        self.username = username
+        self.phone_number = phone_number
+
 
 class UsersPing(Resource):
     def get(self):
